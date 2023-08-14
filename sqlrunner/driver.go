487bf199f6f4d4d8655e6782f1c8003b439711e7
Alan Wootton <alan@gotohere.com>
2023-08-14 19:08:35 +0000

    remove sleep in driver.go

--- sqlrunner/driver.go
+++ sqlrunner/driver.go
@@ -287,7 +287,7 @@ func analyzeRow(proxyConfig ProxyConnect, row []string, validate bool) {
 	} else if statementType == Update {
 		sqlInfo.executeUpdate(db)
 	} else if statementType == Select {
-		time.Sleep(1000 * time.Millisecond)
+		//time.Sleep(500 * time.Millisecond)
 		sqlInfo.executeQuery(db)
 	} else if statementType == Count {
 		//time.Sleep(500 * time.Millisecond)

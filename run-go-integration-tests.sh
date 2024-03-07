ecbe40b79b959c22b0fb9b68286d3cb71c8a7e82
Alan-Wootton <alan@gotohere.com>
2024-03-07 02:59:03 -0800

    edit integration tests

--- run-go-integration-tests.sh
+++ run-go-integration-tests.sh
@@ -23,7 +23,7 @@ echo "starting TestRetainData_Part2 again"
 go test -timeout 90s -run ^TestRetainData_Part2$  github.com/disney/quanta/test
 
 # from sql_test.go
-sleep 5
+sleep 15
 echo "starting TestShowTables"
 go test -timeout 200s -run ^TestShowTables$  github.com/disney/quanta/test
 
@@ -46,14 +46,14 @@ sleep 15
 echo "starting TestLocalBasic3then4"
 go test -timeout 200s -run ^TestLocalBasic3then4$  github.com/disney/quanta/test
 
-sleep 5
-# echo "starting TestLocalBasic4minus1" atw FIXME: fails
-# go test -timeout 300s -run ^TestLocalBasic4minus1$  github.com/disney/quanta/test
-
 # from local3+1_zip_test.go
 sleep 15
 echo "starting TestLocalBasic3then4Zip"
 go test -timeout 200s -run ^TestLocalBasic3then4Zip$  github.com/disney/quanta/test
+
+# sleep 5
+# echo "starting TestLocalBasic4minus1" atw FIXME: fails
+# go test -timeout 300s -run ^TestLocalBasic4minus1$  github.com/disney/quanta/test
 
 # TODO: kill the consul, stop the docker containers, run tests in test-integration.sh
 
